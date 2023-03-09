7cd6990b2d094eed05d976a74ad7e61332238fc6
Carlos Rivera <riveramoralescarlosandres25@gmail.com>
2023-03-08 23:20:17 -0500

    Se quita main con js, se añade footer y se mejora formulario del login

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ let iniciar = document.querySelector("#iniciar")
 let usuario = "carlos"
 let contraseña = "123"
 iniciar.addEventListener('click',(e)=>{
-    
+    let _usuario = document.querySelector("#nombre")
+    let _contraseña = document.querySelector("#contraseña")
+    let main = document.querySelector("#main")
+    if(_usuario.value == usuario && _contraseña.value == contraseña){
+        main.innerHTML = '';
+    }
 })
